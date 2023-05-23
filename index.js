a4c9dd6e923a0d78de67ed520bbf72e68497b103
kulaevMagomed94 <kulayev94@mail.ru>
2023-05-23 15:27:54 +0300

    routers_controllers2

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.use(require("./routes/brands.route"));
+app.use(require("./routes/products.route"));
+
 app.listen(5000, () => {
   console.log("Сервер работает!");
 });
